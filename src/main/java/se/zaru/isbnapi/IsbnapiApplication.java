f8cd758036321daf548d163a7738500e0050d1d8
Zakhida Ruslanova <zakhida_sul@hotmail.se>
2022-09-13 19:58:02 +0200

    Created scaffolding project for web API ISBN validation

---
+++ src/main/java/se/zaru/isbnapi/IsbnapiApplication.java
@@ -0,0 +1,13 @@
+package se.zaru.isbnapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsbnapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsbnapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/zaru/isbnapi/IsbnapiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.zaru.isbnapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsbnapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
